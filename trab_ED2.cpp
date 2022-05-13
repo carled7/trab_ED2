bf8b411eac9dbdf2226023130cb07a8811dd7b11
carled7 <carlos_ed03@outlook.com>
2022-05-13 14:48:04 -0300

    quick sort working

--- trab_ED2.cpp
+++ trab_ED2.cpp
@@ -7,32 +7,90 @@
 
 using namespace std;
 
-void printVector(int* v){
+void troca(int v[], int a,int b){
+
+    int aux = v[a];
+    v[a] = v[b];
+    v[b] = aux;
+}
+
+int particiona(int v[],int s, int e){
+    int pivot = v[s];
+    int i=s;
+    for(int j=s+1; j<=e; j++)
+    {
+        if(v[j]<=pivot)
+        {
+            i++;
+            troca(v,i,j);
+        }
+    }
+    troca(v,s,i);
+    return i;
+
+}
+
+void quickSort(int v[], int s, int e){
+    if (s<e)
+    {
+        int p = particiona(v,s,e);
+        quickSort(v,s,p-1);
+        quickSort(v,p+1,e);
+    }
+}
+
+int vtrLen(int v[]){
+ 
+    int len;
+    for (len = 0; v[len] != 0 || v[len] > 1000; len++);
+    return len - 1;
+        
+}
+
+void printVtr(int* v){
 
     char ignore;
 
-    for (int i = 0; v[i] != 0; i++){
-        printf("\n| %d |\n", v[i]);
+    for (int i = 0; i < vtrLen(v); i++){
+        printf("\n%d: \t | %d |",i+1, v[i]);
     }
 
     printf("\n\nDigite qualquer tecla para sair: ");   
     scanf(" %c", &ignore);
 }
 
+int* cpyVtr(int* scr, int* dest){
+
+    for (int i = 0; i < vtrLen(scr); i++){
+        dest[i] = scr[i];
+    }
+
+    return dest;    
+}
+
 void escolherMetOrdena(int* v){
+
     char metodo;
+    int* aux = (int*) malloc(vtrLen(v) * sizeof(int));
+
     system("clear");
-    printf("\n-----------------------------------------\n");
+    printf("-----------------------------------------\n");
     printf(" [B] - Ordenar Vetor");
     printf("\n-----------------------------------------\n");
-    printf("[Q] - Ordenar com QUICK SORT\n");
-    printf("[M] - Ordenar com MERGE SORT\n");
+    printf(" [Q] - Ordenar com QUICK SORT\n");
+    printf(" [M] - Ordenar com MERGE SORT\n");
     scanf(" %c", &metodo);
 
     switch (metodo)
     {
     case 'Q':
-       // quickSort(&v, 0, v.size());
+        cpyVtr(v, aux);
+        quickSort(aux, 0, vtrLen(aux)-1);
+        system("clear");
+        printf("-----------------------------------------\n");
+        printf(" [Q] - Vetor Ordenado");
+        printf("\n-----------------------------------------\n");
+        printVtr(aux);
         break;
     
     default:
@@ -44,10 +102,10 @@ int* gerarVetor(int* v){
 
     system("clear");
     int len;
-    printf("\n-----------------------------------------\n");
+    printf("-----------------------------------------\n");
     printf(" [A] - Gerar Valores");
     printf("\n-----------------------------------------\n");
-    printf("Digite o tamanho do vetor a ser gerado: ");
+    printf(" Digite o tamanho do vetor a ser gerado: ");
     scanf(" %d", &len);
 
     v = (int*) malloc(len * sizeof(int));
@@ -59,43 +117,41 @@ int* gerarVetor(int* v){
     return v;
 }
 
-void hub(char op, int* vetor){
-    
-    switch (op)
-    {
-    case 'A':
-        vetor = gerarVetor(vetor);
-        printVector(vetor);
-        break;
-    case 'B':
-        //escolherMetOrdena(*vetor);
-        break;
+void menu(int* vetor){
+
+    char op;
+    while (op != 'Q'){
+        system("clear");
+        printf("[A] - Gerar vetor\n");
+        printf("[B] - Ordenar vetor\n");
+        printf("[C] - Busca sequencial\n");
+        printf("[D] - Busca binária\n");
+        printf("[E] - Imprimir vetor\n");
+        printf("\n[Q] - Para encerrar programa\n\n");
+
+        scanf(" %c", &op);
+
+        switch (op)
+        {
+        case 'A':
+            vetor = gerarVetor(vetor);
+            break;
+        case 'B':
+            escolherMetOrdena(vetor);
+            break;
+        case 'E':
+            printVtr(vetor);
+        }
     }
-}
 
-void menu(char * op){
-
-    system("clear");
-    printf("[A] - Gerar vetor\n");
-    printf("[B] - Ordenar vetor\n");
-    printf("[C] - Busca sequencial\n");
-    printf("[D] - Busca binária\n");
-    printf("\n[Q] - Para encerrar programa\n\n");
-
-    scanf(" %c", op);
-   // *op = toupper(*op);
 }
 
 int main(){
 
     int* vetor;
-//    vetor = (int*) malloc(5 * sizeof(int));
-    char option = 1;
 
-    while(option != 'Q'){
-        menu(&option);
-        hub(option, vetor);
-
-    }
+    menu(vetor);
+    //hub(option, vetor);
+    
     printf("\n\nPrograma encerrado com sucesso\n\n\n");
 }
